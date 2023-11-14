b0ab7c8c5329e03416188e25113698211ee5f66c
ElenoreGill <elenoregill55@gmail.com>
2023-11-14 12:08:06 +0100

    sections et fonts

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+gsap.to(".slider-track", {
+  scrollTrigger: {
+    trigger: ".slider-mask",
+    start: "top top",
+    end: "+=300%",
+    pin: true,
+    scrub: true,
+    markers: true,
+  },
+  x: "-75%",
+  ease: "sine.inOut",
+});
